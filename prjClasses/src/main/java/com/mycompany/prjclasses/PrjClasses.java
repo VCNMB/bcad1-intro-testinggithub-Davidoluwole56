a679fdab945d401c7a869046391151ad2a38b53a
Davidoluwole56 <ST10483365@vcconnect.edu.za>
2025-03-11 11:41:20 +0200

    Connecting CLasses with main project

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -4,15 +4,34 @@
 
 package com.mycompany.prjclasses;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author lab_services_student
  */
 public class PrjClasses {
+    private static Student s = new Student();
 
     public static void main(String[] args) {
-        newClass nc = new newClass();
-        nc.helloClass( ); //have to use variable name nc to comunicate with the class
-       System.out.println(nc.strSecondClassVar); //output variable from second class
+        String strName = JOptionPane.showInputDialog(null,"Please enter student name");  
+        String strCourse = JOptionPane.showInputDialog(null,"Please enter student course");   
+        double dblAvg = Double.parseDouble(JOptionPane.showInputDialog(null,"Please enter your average"));   
+        
+        s.setName(strName);
+        s.setCourse(strCourse);
+        s.setAverage(dblAvg);
+        print();
+        
     }
+    public static void print(){
+        
+        String strDisplay = "The updated student average results are: \n" + 
+                "STUDENT: " + s.getName() + "\n" +
+                "COURSE: " + s.getCours() + "\n" +
+                "UPDATED AVEAGE: " + s.getAverage()+ "%" ;
+        
+        JOptionPane.showMessageDialog(null, strDisplay);
+    }
+    
 }
